9cfe88253d351c87b63b11efd2f2378af39bd869
Trevor Seitz <trevinla@gmail.com>
2018-03-12 22:25:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,7 @@ end
 def batch_badge_creator(speaker_list)
   speaker_list.each{|name| badges = badge_maker(name)}
   # speaker_list.each{|name| return badge_maker(name)}
+  return badges
 end
 
 def assign_rooms(speaker_list)
