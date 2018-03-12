f683e080dd81e685591ef92e3b03e564438465f4
Trevor Seitz <trevinla@gmail.com>
2018-03-12 21:28:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+
+# def conference_badges(speaker_list)
+  badges = []
+  room_assignments = []
+  # speaker_list.each_with_index{|name, index| badge_maker(name)}
+# end
+
+def batch_badge_creator(speaker_list)
+  speaker_list.each_with_index{|name, index| badges << badge_maker(name)}
+  return badges
+end
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def assign_rooms(speaker_list)
+  # rooms = []
+  speaker_list.each_with_index{|name, index| room_assignments[index] << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+  return room_assignments
+end
+
+def printer(badges, room_assignments)
+  print badges
+  print room_assignments
+end
